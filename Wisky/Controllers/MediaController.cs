4699894d888d7d33454643d89b655224c37f02af
tantmse61751 <tantmse61751@gmail.com>
2018-06-14 11:00:30 +0700

    [PLAYLIST] new source tree

---
+++ Wisky/Controllers/MediaController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DSS.Controllers
+{
+    public class MediaController : Controller
+    {
+        // GET: Media/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // Media/Form
+        public ActionResult Form()
+        {
+            return View();
+        }
+    }
+    
+}
