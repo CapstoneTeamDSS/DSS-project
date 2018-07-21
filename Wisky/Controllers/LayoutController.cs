34b4bc4aec7610368e5cf6173437d203c7655534
anntvse62109 <vienan123@gmail.com>
2018-07-21 13:58:40 +0700

    [Layout] add layoutview

---
+++ Wisky/Controllers/LayoutController.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using DSS.Data.Models.Entities.Services;
+using SkyWeb.DatVM.Mvc.Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DSS.Controllers
+{
+    public class LayoutController : Controller
+    {
+        //[Authorize]
+
+        ILayoutService layoutService = DependencyUtils.Resolve<ILayoutService>();
+        IMapper mapper = DependencyUtils.Resolve<IMapper>();
+
+        //GET: Layout/Index
+        public ActionResult Index()
+        {
+            var layouts = this.layoutService.Get().ToList();
+            var layoutVMs = new List<Models.LayoutDetailVM>();
+
+            foreach (var item in layouts)
+            {
+                var b = new Models.LayoutDetailVM
+                {
+                    Id = item.LayoutID,
+                    Description = item.Description,
+                    IsHorizontal = item.isHorizontal,
+                    IsPublic =  item.isPublic,
+                    Layoutsrc = item.LayoutSrc,
+                    Title = item.Title,
+                    Url = item.URL
+                };
+                layoutVMs.Add(b);
+            }
+            ViewBag.layoutsList = layoutVMs;
+            return View();
+        }
+    }     
+    }
