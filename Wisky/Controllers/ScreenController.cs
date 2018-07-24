c77ef6563fc0cd64b7f255394e7d12c66d07e659
toxuantoan96 <toxuantoan96@gmail.com>
2018-07-24 13:16:37 +0700

    [SCREEN] done show warning when delete

--- Wisky/Controllers/ScreenController.cs
+++ Wisky/Controllers/ScreenController.cs
@@ -112,6 +112,39 @@ namespace DSS.Controllers
             }
             return View("Form", model);
         }
+        // POST: Screen/CheckScreenIdIsMatching  
+        [HttpPost]
+        public JsonResult CheckScreenIdIsMatching(int id)
+        {
+            try
+            {
+                //Get device by screen Id
+                IDeviceService deviceService = DependencyUtils.Resolve<IDeviceService>();
+                var device = deviceService.Get(a => a.ScreenID == id).FirstOrDefault();
+                //bool isUsing = true;
+                //if (device == null)
+                //{
+                //    isUsing = false;
+                //}
+                DSS.Models.MatchingDeviceVM deviceVM = null;
+                if (device != null)
+                {
+                    deviceVM = new DSS.Models.MatchingDeviceVM
+                    {
+                        Title = device.Title,
+                    };
+                }
+                return Json(new
+                {
+                    isUsing = device != null,
+                    deviceVM = deviceVM,
+                }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
         // POST: Screen/Update
         [HttpPost]
         public async System.Threading.Tasks.Task<ActionResult> Update(Models.ScreenVM model)
