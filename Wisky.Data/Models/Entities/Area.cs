e0f0e88042fd4ec026fa0faef6fe22dbf85baee9
toxuantoan96 <toxuantoan96@gmail.com>
2018-08-13 16:23:29 +0700

    fix conflict

--- Wisky.Data/Models/Entities/Area.cs
+++ Wisky.Data/Models/Entities/Area.cs
@@ -17,7 +17,6 @@ namespace DSS.Data.Models.Entities
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public Area()
         {
-            this.Scenarios = new HashSet<Scenario>();
             this.ScenarioItems = new HashSet<ScenarioItem>();
         }
     
@@ -27,8 +26,6 @@ namespace DSS.Data.Models.Entities
     
         public virtual Layout Layout { get; set; }
         public virtual VisualType VisualType { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<Scenario> Scenarios { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<ScenarioItem> ScenarioItems { get; set; }
     }
--- Wisky.Data/Models/Entities/Scenario.cs
+++ Wisky.Data/Models/Entities/Scenario.cs
@@ -30,7 +30,6 @@ namespace DSS.Data.Models.Entities
         public Nullable<bool> isPublic { get; set; }
         public int AudioArea { get; set; }
     
-        public virtual Area Area { get; set; }
         public virtual Brand Brand { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<DeviceScenario> DeviceScenarios { get; set; }
