3646bf1322c62fc97754a67d63d084aff14b0994
anntvse62109 <vienan123@gmail.com>
2018-08-17 15:25:31 +0700

    [functions] DONE FRONT-END DASHBOARD

--- Wisky/Controllers/HomeController.cs
+++ Wisky/Controllers/HomeController.cs
@@ -11,11 +11,15 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
+using DSS.Data.Models.Entities.Services;
+using DSS.Controllers;
+
 namespace Wisky.Controllers
 {
     [Authorize]
     public class HomeController : BaseController
     {
+
         public ActionResult Index()
         {
             var user = System.Web.HttpContext.Current.User;
@@ -23,8 +27,15 @@ namespace Wisky.Controllers
             ViewBag.FullName = user.Identity.Name;
             Session["username"] = user.Identity.Name;
             Session["userID"] = user.Identity.GetUserId(); //GetUserId() only can be used when using Microsoft.AspNet.Identity;
+            //Get Size Account
+            IAspNetUserService aspNetUserService = DependencyUtils.Resolve<IAspNetUserService>();
+            var users = Helper.GetCurrentUser();
+            var accountList = aspNetUserService.GetAccountsByBrandId(users.BrandID);
+            var size = accountList.Count;
+            ViewBag.accountList = size;
             return View();
         }
+
 
     }
 }
