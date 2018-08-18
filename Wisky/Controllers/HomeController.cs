79c33817018e71d3105097e7baa95645ced4fb88
anntvse62109 <vienan123@gmail.com>
2018-08-18 11:00:45 +0700

    [functions] Done backend dashboard

--- Wisky/Controllers/HomeController.cs
+++ Wisky/Controllers/HomeController.cs
@@ -33,7 +33,57 @@ namespace Wisky.Controllers
             var accountList = aspNetUserService.GetAccountsByBrandId(users.BrandID);
             var size = accountList.Count;
             ViewBag.accountList = size;
+            //ScreenList
+            IScreenService screenSevice = DependencyUtils.Resolve<IScreenService>();
+            var screenList = screenSevice.GetScreenIdByBrandId(users.BrandID);
+            var screenSize = screenList.Count;
+            ViewBag.screenList = screenSize;
+
+            //BoxList
+            IBoxService boxService = DependencyUtils.Resolve<IBoxService>();
+            var boxList = boxService.GetBoxIdByBrandId(users.BrandID);
+            var boxSize = boxList.Count;
+            ViewBag.boxList = boxSize;
+
+            //DeviceList
+            IDeviceService deviceService = DependencyUtils.Resolve<IDeviceService>();
+            var deviceList = deviceService.GetDeviceByBrandId(users.BrandID);
+            var deviceSize = deviceList.Count;
+            ViewBag.deviceList = deviceSize;
+
+            //LocationList
+            ILocationService locationService = DependencyUtils.Resolve<ILocationService>();
+            var locationList = locationService.GetLocationIdByBrandId(users.BrandID);
+            var locationSize = locationList.Count;
+            ViewBag.locationList = locationSize;
+
+            //MediaSrcList
+            IMediaSrcService mediaSrcService = DependencyUtils.Resolve<IMediaSrcService>();
+            var mediasrcList = mediaSrcService.GetMediaSrcByBrand(users.BrandID);
+            var mediasrcSize = mediasrcList.Count;
+            ViewBag.mediarsList = mediasrcSize;
+
+            //PlaylistList
+            IPlaylistService playlistService = DependencyUtils.Resolve<IPlaylistService>();
+            var playlistList = playlistService.GetPlaylistIdByBrandId(users.BrandID);
+            var playlistSize = playlistList.Count;
+            ViewBag.playlistList = playlistSize;
+
+            //ScenarioList
+            IScenarioService  scenarioService= DependencyUtils.Resolve<IScenarioService>();
+            var scenarioList = scenarioService.GetScenarioIdByBrandId(users.BrandID);
+            var scenarioSize = scenarioList.Count;
+            ViewBag.scenarioList = scenarioSize;
+
+            //ScheduleList
+            IScheduleService scheduleService = DependencyUtils.Resolve<IScheduleService>();
+            var scheduleList = scheduleService.GetScheduleIdByBrandId(users.BrandID);
+            var scheduleSize = scheduleList.Count;
+            ViewBag.scheduleList = scheduleSize;
             return View();
+            
+           
+          
         }
 
 
