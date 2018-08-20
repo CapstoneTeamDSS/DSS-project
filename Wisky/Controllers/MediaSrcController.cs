f7c944ffac4beb6382c5cb48716bedb3bc91e077
toxuantoan96 <toxuantoan96@gmail.com>
2018-08-20 22:01:11 +0700

    [ANDROID_BOX] Done show Add/Update notification.[MEDIA_SRC]Done show Add/Update notification

--- Wisky/Controllers/MediaSrcController.cs
+++ Wisky/Controllers/MediaSrcController.cs
@@ -21,6 +21,9 @@ namespace DSS.Controllers
         public ActionResult Index()
         {
             ViewBag.mediaSrcList = GetMediaSrcListByBrandId();
+            ViewBag.addSuccess = Session["ADD_RESULT"] ?? false;
+            ViewBag.updateSuccess = Session["UPDATE_RESULT"] ?? false;
+            Session.Clear();
             return View();
         }
 
@@ -205,6 +208,8 @@ namespace DSS.Controllers
                     SecurityHash = model.SecurityHash
                 };
                 await this.mediaSrcService.CreateAsync(media);
+                Session.Clear();
+                Session["ADD_RESULT"] = true;
                 return Json(new
                 {
                     success = true,
@@ -348,7 +353,8 @@ namespace DSS.Controllers
                     mediaSrc.SecurityHash = mediaSrc.SecurityHash;
                 };
                 await this.mediaSrcService.UpdateAsync(mediaSrc);
-                //return this.RedirectToAction("Index");
+                Session.Clear();
+                Session["UPDATE_RESULT"] = true;
                 return new ContentResult
                 {
                     Content = string.Format("<script type='text/javascript'>window.parent.location.href = '{0}';</script>", Url.Action("Index", "MediaSrc")),
